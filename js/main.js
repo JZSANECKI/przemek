738a20afb741c285076a0bfb403dcffd0aef4684
JZSANECKI <115653252+JZSANECKI@users.noreply.github.com>
2023-02-06 13:45:05 +0100

    3 files modified

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ function reveal() {
   for (var i = 0; i < reveals.length; i++) {
     var windowHeight = window.innerHeight;
     var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 0;
+    var elementVisible = 50;
 
     if (elementTop < windowHeight - elementVisible) {
       reveals[i].classList.add('active');
@@ -15,3 +15,21 @@ function reveal() {
 }
 
 window.addEventListener('scroll', reveal);
+
+function reveal2() {
+  var reveals = document.querySelectorAll('.reveal2');
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 50;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add('active');
+    } else {
+      // reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener('scroll', reveal2);
